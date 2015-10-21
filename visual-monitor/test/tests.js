c97586786859a7aa10019971335c919957f98ae9
DavidKohav <david@gizra.com>
2015-10-21 12:38:07 +0300

    Add a visual monitor.

--- visual-monitor/test/tests.js
+++ visual-monitor/test/tests.js
@@ -63,10 +63,22 @@ describe('Visual monitor testing', function() {
       .url(baseUrl)
       .webdrivercss(testName + '.homepage', {
         name: '1',
-        exclude: [],
-        remove: [],
-        hide: [],
-        screenWidth: selectedCaps == 'chrome' ? [640, 960, 1200] : undefined,
+        exclude:
+          [
+            // Top carousel.
+            '.postsCarousel'
+          ],
+        remove:
+          [
+            // Cookie.
+            '#cookie-law-info-bar'
+          ],
+        hide:
+          [
+            // Company news date.
+            '.teaser_box_content p i',
+          ],
+        screenWidth: selectedCaps == 'chrome' ? [960] : undefined,
       }, resultsCallback)
       .call(done);
   });
